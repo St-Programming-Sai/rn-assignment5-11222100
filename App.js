bb926ad874b72aa7ef9a87f5affbd1d69af530a9
St-Programming-Sai <inlmensah_lartey@st.ug.edu.gh>
2024-06-25 12:20:15 +0000

    adding Main component

--- App.js
+++ App.js
@@ -1,15 +1,66 @@
-import { View, Text } from "react-native";
+// App.js
+
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import PageOne from './components/FirstPage';
+import PageTwo from './components/SecondPage';
+import { ThemeProvider, useTheme } from './components/ThemeContext';
+import { Image } from 'react-native';
+import MyCardsPage from './components/MyCards';
+import StatisticsPage from './components/Statistics';
+
+const Tab = createBottomTabNavigator();
+
+// Function to get tab bar icon based on route name and focus state
+const getTabBarIcon = (routeName, focused, theme) => {
+  let iconImage;
+
+  if (routeName === 'Home') {
+    iconImage = focused ? require('./assets/home.png') : require('./assets/inactiveHome.png');
+  } else if (routeName === 'Settings') {
+    iconImage = focused ? require('./assets/settings.png') : require('./assets/inactiveSettings.png');
+  }else if (routeName === 'My Cards') {
+    iconImage = focused ? require('./assets/myCards.png') : require('./assets/inactiveMyCards.png');
+  }else if (routeName === 'Statistics') {
+    iconImage = focused ? require('./assets/statictic.png') : require('./assets/inactiveStatictics.png');
+  }
+
+  return <Image source={iconImage} style={{ width: 25, height: 25 }} />;
+};
+
+const MyTabs = () => {
+  const { theme } = useTheme(); // Accessing theme from ThemeContext
+
+  return (
+    <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused }) => getTabBarIcon(route.name, focused, theme),
+      tabBarActiveTintColor: '#4169E1',
+      tabBarInactiveTintColor: 'gray',
+      tabBarStyle: {
+        backgroundColor: theme === 'dark' ? '#333333' : '#f2f2f2',
+      },
+    })}
+    tabBarOptions={{
+      activeTintColor: theme === 'dark' ? 'white' : 'black', // Color of the active tab label/icon
+      inactiveTintColor: 'gray', // Color of the inactive tab label/icon
+    }}
+    >
+      <Tab.Screen name="Home" component={PageOne} options={{ headerShown: false }} />
+      <Tab.Screen name="My Cards" component={MyCardsPage} options={{ headerShown: false }} />
+      <Tab.Screen name="Statistics" component={StatisticsPage} options={{ headerShown: false }}/>
+      <Tab.Screen name="Settings" component={PageTwo} options={{ headerShown: false }}/>
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
+    <ThemeProvider>
+      <NavigationContainer>
+        <MyTabs />
+      </NavigationContainer>
+    </ThemeProvider>
   );
 }
